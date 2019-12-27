74a7bc43403f0abc88bda1d10a4b4ef827b2e182
Behtash Moradi <behtash.moradi@live.com>
2019-12-27 15:02:03 +1100

    handling event-continue'

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.css";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
     tags: ["tag1", "tag2", "tag3"]
   };
   styles = {
@@ -42,7 +42,7 @@ class Counter extends Component {
         </button>
         <button
           className="btn btn-danger btn-sm m-2"
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
         >
           Delete
         </button>
