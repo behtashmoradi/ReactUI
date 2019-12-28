2e234bd959a74d5cf30fcf7256adcf8c8b4737ce
Behtash Moradi <behtash.moradi@live.com>
2019-12-29 08:37:47 +1100

    use object destrucure for code simplicity

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,20 +8,21 @@ class Counter extends Component {
   };
   handleReset;
   render() {
+    const { id, counter, onIncrement, onDelete } = this.props;
     console.log(this.props);
     return (
       <div>
-        <h4>{this.props.id}</h4>
+        <h4>{id}</h4>
         <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
         <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
+          onClick={() => onIncrement(counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
         <button
           className="btn btn-danger btn-sm m-2"
-          onClick={() => this.props.onDelete(this.props.counter.id)}
+          onClick={() => onDelete(id)}
         >
           Delete
         </button>
